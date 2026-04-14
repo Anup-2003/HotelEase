697bc5b27d44de50580ab109703417974242f174
Anup Gupta <anupgupta998466@gmail.com>
2026-04-15 02:53:45 +0530

    Update port binding and node engine for Render deployment

--- app.js
+++ app.js
@@ -118,6 +118,6 @@ app.use((err, req, res, next) => {
  // res.status(statusCode).send(message);
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("Server is listening on port 8080")
 });
